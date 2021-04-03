9afd390fdbfc711ee7ec4b6e6a1aa686701d9d3a
jclagoria <lagoria79@gmail.com>
2021-04-02 23:08:57 -0300

    First commit with the projects templates

---
+++ movie-catalog-service/src/test/java/ar/com/lagoria/movie/catalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.lagoria.movie.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
